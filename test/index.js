651dba4c5cb3b77d28d468fca8d61754dbf7098e
Tyler Mizuyabu <tyler@monanetworks.com>
2017-09-19 17:01:45 -0400

    removed temp folder

--- test/index.js
+++
@@ -1,5 +0,0 @@
-const { NotificationService } = require('../dist');
-console.log(NotificationService);
-const notificationService = new NotificationService({API_PREFIX:'test'});
-
-notificationService.getUser({userId:'101'});
