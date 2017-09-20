f2cbe34b3aece88d7c9dc7d2c7dc9852e4570c48
Tyler Mizuyabu <tyler@monanetworks.com>
2017-09-20 16:01:27 -0400

    fixed group-api typos

--- src/api/group-api.js
+++ src/api/group-api.js
@@ -4,13 +4,13 @@ import config from '../config';
 export default {
   addUserToGroup: async ({ userId, group }) =>
     await command({
-      uri: `${config.API_PREFIX}group/${userId}/${group}`,
+      uri: `${config.API_PREFIX}/group/${userId}/${group}`,
       method: 'POST',
     }),
-  getGroup: async ({ group }) => await command({ uri: `${config.API_PREFIX}group/${group}`, method: 'GET' }),
+  getGroup: async ({ group }) => await command({ uri: `${config.API_PREFIX}/group/${group}`, method: 'GET' }),
   removeUserFromGroup: async ({ userId, group }) =>
     await command({
-      uri: `${config.API_PREFIX}group/${userId}/${group}`,
+      uri: `${config.API_PREFIX}/group/${userId}/${group}`,
       method: 'DELETE',
     }),
 };
